c5f7375b3fd3e2783c244bcf80a014cf44e153a7
Walter Nicanor <padillanovoaw@iestptrujillo.net>
2024-09-19 11:03:24 -0500

    mi primer commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebApI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("DevolverEdad")]
+        public int DevolverEdad()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno3 = new Alumno("Juan", "perez", "99283478",new DateTime(2005,08,30));
+            return Alumno3.DevolverEdad();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Controllers/alumno.cs
@@ -0,0 +1,60 @@
+﻿namespace WebApI.Controllers
+{
+    public class Alumno
+    {
+        //creando propiedades
+        string Nombre;
+        string apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+
+
+        //constructor sin parametros
+        public Alumno()
+        {
+            Nombre = string.Empty;
+            apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+
+        }
+
+
+        //constructor con parametros
+        public Alumno(string pnombre, string papellido, string pdni)
+        {
+            Nombre = pnombre;
+            apellido = papellido;
+            dni = pdni;
+        }
+
+        //constructor con parametros pero con fechadenacimiento
+        public Alumno(string pnombre, string papellido, string pdni, DateTime pfechanacimiento)
+        {
+            Nombre = pnombre;
+            apellido = papellido;
+            dni = pdni;
+            fechanacimiento = pfechanacimiento;
+        }
+
+
+
+
+
+
+        public string DevolverNombreCompleto()
+        {
+            return dni;
+        }
+
+
+        public int DevolverEdad()
+        { 
+            return DateTime.Now.Year - fechanacimiento.Year;
+        }
+
+
+        
+    }
+}
